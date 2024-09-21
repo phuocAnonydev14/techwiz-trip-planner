ded7a86efb3645254ee093887b0fec7df9890b40
anonyDev <phuochh@nortstudio.vn>
2024-09-22 02:37:15 +0700

    feat: test

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,11 @@ export default defineConfig({
     process.env.INLINE ? viteSingleFile() : null,
   ].filter(Boolean),
 });
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
